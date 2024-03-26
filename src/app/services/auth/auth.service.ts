b3e65446a20ad9215e61c8f43a2e440a51a3f60d
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2024-03-27 00:07:39 +0300

    lint

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -7,15 +7,12 @@ import { UtilsService } from '../../shared/services/utils/utils.service';
 import { USER } from '@constants/localstorage-keys';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   #user$: BehaviorSubject<IUser> = new BehaviorSubject<IUser>(undefined);
 
-  constructor(
-    private readonly utils: UtilsService
-  ) { }
+  constructor(private readonly utils: UtilsService) { }
 
   set user(user: IUser) {
     this.#user$.next(user);
@@ -38,20 +35,22 @@ export class AuthService {
     const userAsString = localStorage.getItem(USER);
     const user = this.utils.isJsonString<IUser>(userAsString);
     this.user = user;
-  }
+  };
 
   getUserByEmailAndPassword = (loginCredential: ILogin): IUser | undefined => {
-    const userWithPassword: IUser | undefined = USERS.find(user => user.password === loginCredential.password);
+    const userWithPassword: IUser | undefined = USERS.find(
+      (user) => user.password === loginCredential.password
+    );
 
     if (!userWithPassword) {
       return undefined;
     }
-    const { password, ...user } = userWithPassword
+    const { password, ...user } = userWithPassword;
     this.user = user;
     return user;
-  }
+  };
 
   logout = () => {
     this.user = undefined;
-  }
+  };
 }
